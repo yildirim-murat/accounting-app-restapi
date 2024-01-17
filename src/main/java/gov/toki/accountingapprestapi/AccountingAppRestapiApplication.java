b13cdc862c220ee623488eb48752ef035431e4c8
yildirim-murat <muratyildirim.work@gmail.com>
2024-01-17 12:01:58 +0300

    First Commit

---
+++ src/main/java/gov/toki/accountingapprestapi/AccountingAppRestapiApplication.java
@@ -0,0 +1,13 @@
+package gov.toki.accountingapprestapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccountingAppRestapiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AccountingAppRestapiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/gov/toki/accountingapprestapi/AccountingAppRestapiApplicationTests.java
@@ -0,0 +1,13 @@
+package gov.toki.accountingapprestapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccountingAppRestapiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
